e456d2d97d1c9a229570dacba25ffaa7be1ef0d5
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2022-10-27 15:48:55 +0600

    Now it works more corectly by logic

--- script.js
+++ script.js
@@ -1,52 +1,40 @@
 var buttons = document.querySelectorAll("main button");
 var circle = document.querySelector("#circle");
 var cross = document.querySelector("#cross");
-var chooseButtons = [circle, cross];
 var reset = document.querySelector(".reset");
-var modal = document.querySelector(".modal");
-var activeObj;
+var activeObj = cross;
+var clickCounter = 0;
 
 var selectActiveObj = function (element) {
-    for (let i = 0; i < chooseButtons.length; i++) {
-        chooseButtons[i].classList.remove("active");
-    }
+    cross.classList.remove("active");
+    circle.classList.remove("active");
     element.classList.add("active");
     activeObj = element;
 };
 
-circle.addEventListener('click', function () {selectActiveObj(circle);});
-cross.addEventListener('click', function () {selectActiveObj(cross);});
+circle.addEventListener('click', function () {selectActiveObj(circle);cross.disabled = true;circle.disabled = true;});
+cross.addEventListener('click', function () {selectActiveObj(cross);cross.disabled = true;circle.disabled = true;});
 
-var changeActiveObj = function () {
+var changeObj = function () {
     if (activeObj == circle) {
         selectActiveObj(cross);
     } else {
         selectActiveObj(circle);
     }
-};
+}
 
 var addClickToButton = function (element) {
     element.addEventListener('click', function () {
+        cross.disabled = true;
+        circle.disabled = true;
         element.classList.add(activeObj.dataset.obj);
         element.disabled = true;
+        clickCounter++;
         checkForWin(activeObj.dataset.obj);
-        if (activeObj == circle) {
-            selectActiveObj(cross);
-        } else {
-            selectActiveObj(circle);
-        }
     });
 };
 
-reset.addEventListener('click', function () {
-    clear();
-    selectActiveObj(cross);
-});
-
-modal.addEventListener('click', function () {
-    modal.style.display = "none";
-    clear();
-});
+reset.addEventListener('click', function () {clear();});
 
 var clear = function () {
     for (let i = 0; i < buttons.length; i++) {
@@ -54,32 +42,54 @@ var clear = function () {
         buttons[i].classList.remove("cross");
         buttons[i].disabled = false;
     }
+    clickCounter = 0;
+    cross.disabled = false;
+    circle.disabled = false;
+    selectActiveObj(cross);
 }
 
 var checkForWin = function (obj) {
     var i = buttons;
-    if (i[0].className == i[1].className && i[0].className == i[2].className && i[0].className == obj) {showModal();}
-    else if (i[3].className == i[4].className && i[3].className == i[5].className && i[3].className == obj) {showModal();}
-    else if (i[6].className == i[7].className && i[6].className == i[8].className && i[6].className == obj) {showModal();}
+    if (i[0].className == i[1].className && i[0].className == i[2].className && i[0].className == obj) {roundEnds();}
+    else if (i[3].className == i[4].className && i[3].className == i[5].className && i[3].className == obj) {roundEnds();}
+    else if (i[6].className == i[7].className && i[6].className == i[8].className && i[6].className == obj) {roundEnds();}
     //horizontal
 
-    else if (i[0].className == i[3].className && i[0].className == i[6].className && i[0].className == obj) {showModal();}
-    else if (i[1].className == i[4].className && i[1].className == i[7].className && i[1].className == obj) {showModal();}
-    else if (i[2].className == i[5].className && i[2].className == i[8].className && i[2].className == obj) {showModal();}
+    else if (i[0].className == i[3].className && i[0].className == i[6].className && i[0].className == obj) {roundEnds();}
+    else if (i[1].className == i[4].className && i[1].className == i[7].className && i[1].className == obj) {roundEnds();}
+    else if (i[2].className == i[5].className && i[2].className == i[8].className && i[2].className == obj) {roundEnds();}
     //vertical
 
-    else if (i[0].className == i[4].className && i[0].className == i[8].className && i[0].className == obj) {showModal();}
-    else if (i[2].className == i[4].className && i[2].className == i[6].className && i[2].className == obj) {showModal();}
-    //cross
+    else if (i[0].className == i[4].className && i[0].className == i[8].className && i[0].className == obj) {roundEnds();}
+    else if (i[2].className == i[4].className && i[2].className == i[6].className && i[2].className == obj) {roundEnds();}
+    //diagonal
+
+    else {
+        changeObj();
+        if (clickCounter === 9) {
+            roundEnds(clickCounter);
+        }
+    }
 };
 
-var showModal = function () {
+var roundEnds = function (clickCounter) {
+    if (clickCounter === 9) {
+        var modal = document.querySelector(".draw");
+    } else {
+        var modal = document.querySelector(".win");
+        modal.children[0].children[0].src = activeObj.children[0].src;
+    }
     modal.style.display = "block";
-    modal.children[0].children[0].src = activeObj.children[0].src;
+    restartGame(modal);
+}
+
+var restartGame = function (element) {
+    element.addEventListener('click', function () {
+        element.style.display = "none";
+        clear();
+    });
 }
 
 for (let i = 0; i < buttons.length; i++) {
     addClickToButton(buttons[i]);
 }
-
-selectActiveObj(cross);
